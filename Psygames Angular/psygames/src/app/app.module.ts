1faeb92755c08fb58aa3efc2ca1465c5886fb14a
CaioLSDev <107144210+CaioLSDev@users.noreply.github.com>
2022-09-28 19:06:35 -0300

    Add files via upload

---
+++ Psygames Angular/psygames/src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { AppRoutingModule } from "./app-routing/app-routing.module";
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { HomeComponent } from './views/home/home.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { LoginComponent } from './views/login/login.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    LoginComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    NgbModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
